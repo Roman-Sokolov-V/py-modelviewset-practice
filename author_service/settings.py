98cd08ef324c914560c63f48a9cbd28d4399e974
Roman Sokolov <gnonasis@gmail.com>
2024-12-10 10:26:46 +0200

    Solution

--- author_service/settings.py
+++ author_service/settings.py
@@ -22,7 +22,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "very_sekret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
