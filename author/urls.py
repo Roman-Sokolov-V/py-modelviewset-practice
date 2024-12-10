a16f023e07a8cef34e825abefc2098cba6f97430
Roman Sokolov <gnonasis@gmail.com>
2024-12-10 10:19:32 +0200

    Solution

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,29 @@
-# Create your urls here
+from django.urls import path, include
 
-urlpatterns = []
+from rest_framework import routers
+
+from author.views import AuthorViewSet
+
+
+urlpatterns = [
+    path(
+        "authors/",
+        AuthorViewSet.as_view({"get": "list", "post": "create"}),
+        name="manage"
+    ),
+    path(
+        "authors/<int:pk>/",
+        AuthorViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy"
+            }
+        ),
+        name="manage-detail"
+
+    )
+]
+
+app_name = "author"
--- author_service/settings.py
+++ author_service/settings.py
@@ -11,24 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-4l*aqy-h!k0a$)68da!_zw#x8)jo0r&!5aqd9gcv9z3zr)h)*="
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
@@ -128,4 +127,4 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
